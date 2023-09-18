044e1f1e4216cc42efa7564abaaa802613806c13
Oliver Armfelt <oliver.armfelt@clear.bank>
2023-09-18 16:42:49 +0100

    Set up ci pipline

---
+++ NextValueTests/Example.cs
@@ -0,0 +1,19 @@
+﻿namespace NextValueTests;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class Example
+{
+    [Test]
+    public void ShouldPass()
+    {
+        Assert.Pass("Should Pass");
+    }
+
+    [Test]
+    public void ShouldFail()
+    {
+        Assert.Fail("Should Fail");
+    }
+}
