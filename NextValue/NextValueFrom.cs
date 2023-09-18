e1c22b8597f266de9245415314697ded30d4bbd3
Oliver Armfelt <oliver.armfelt@clear.bank>
2023-09-18 10:00:21 +0100

    Support enums and items from a list

---
+++ NextValue/NextValueFrom.cs
@@ -0,0 +1,22 @@
+﻿namespace NextValue;
+using System.Collections.Generic;
+
+public static class NextValueFrom
+{
+    public static T From<T>(this NextValue next, IEnumerable<T> list)
+    {
+        var vals = list.ToArray();
+        if (!vals.Any()) throw new ArgumentException(nameof(list), "List must have some values to select one from");
+        return vals.ElementAt((next.Int() - 1) % vals.Length)!;
+    }
+
+    public static T Enum<T>(this NextValue next)
+       where T : Enum
+    {
+        // presuming enums are int based and never pick a zero
+        var vals = System.Enum.GetValues(typeof(T)).Cast<int>().Where(x => x != 0);
+        if (!vals.Any()) throw new ArgumentException($"Enum must have some values to select one from - '{(T)(object)0}' is ignored");
+        var val = next.From<int>(vals);
+        return (T)(object)val;
+    }
+}
---
+++ NextValueTests/NextValueFromTests.cs
@@ -0,0 +1,86 @@
+﻿namespace NextValueTests;
+
+using NextValue;
+
+public class NextValueFromTests
+{
+    [Test]
+    public void NextValue_From_initial_value_without_seed_is_first_value()
+    {
+        var options = Enumerable.Range(1, 10).ToArray();
+        var nextValue = new NextValue();
+        Assert.That(nextValue.From(options), Is.EqualTo(1));
+
+    }
+
+    [Test]
+    public void NextValue_From_initial_value_with_seed_has_seed_value()
+    {
+        var options = Enumerable.Range(1, 10).ToArray();
+        var nextValue = new NextValue(102);
+        Assert.That(nextValue.From(options), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void NextValue_From_values_have_index_increasing()
+    {
+        var options = Enumerable.Range(1, 10).ToArray();
+        var nextValue = new NextValue();
+        var values = Enumerable.Range(1, 3).Select(i => nextValue.From(options)).ToArray();
+        Assert.That(values, Is.EqualTo(new[] { 1, 2, 3 }));
+
+    }
+
+    [Test]
+    public void NextValue_From_when_options_are_empty_expect_exception()
+    {
+        var options = Array.Empty<int>();
+        var nextValue = new NextValue();
+        Assert.That(() => nextValue.From(options), Throws.ArgumentException.With.Message.Match("List must have some values to select one from"));
+    }
+
+    [Test]
+    public void NextValue_Enum_initial_value_without_seed_is_first_value()
+    {
+        var nextValue = new NextValue();
+        Assert.That(nextValue.Enum<TestEnum>(), Is.EqualTo(TestEnum.One));
+    }
+
+    [Test]
+    public void NextValue_Enum_initial_value_with_seed_is_seed_value()
+    {
+        var nextValue = new NextValue(102);
+        Assert.That(nextValue.Enum<TestEnum>(), Is.EqualTo(TestEnum.Two));
+    }
+
+    [Test]
+    public void NextValue_Enum_values_have_index_increasing()
+    {
+        var nextValue = new NextValue(2);
+        var values = Enumerable.Range(1, 6).Select(i => nextValue.Enum<TestEnum>()).ToArray();
+        Assert.That(values, Is.EqualTo(new[] { TestEnum.Two, TestEnum.Three, TestEnum.Ten, TestEnum.One, TestEnum.Two, TestEnum.Three }));
+
+    }
+
+
+    [Test]
+    public void NextValue_Enum_when_values_are_empty_expect_exception()
+    {
+        var nextValue = new NextValue();
+        Assert.That(() => nextValue.Enum<TestEmptyEnum>(), Throws.ArgumentException.With.Message.Match("Enum must have some values to select one from - 'Unknown' is ignored"));
+    }
+
+    private enum TestEmptyEnum
+    {
+        Unknown = 0,
+    }
+
+    private enum TestEnum
+    {
+        Unknown = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Ten = 10,
+    }
+}
